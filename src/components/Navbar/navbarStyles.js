e71c7cd2e8618e892df906da42f85e0c92031114
Alex Timofeev <asashek@yandex.ru>
2021-10-09 20:29:13 +0300

    navbar link fix

--- src/components/Navbar/navbarStyles.js
+++ src/components/Navbar/navbarStyles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles((theme) => ({
     navLink: {
-        color: "#fff",
+        color: "#fff  !important",
         paddingRight: '20px',
         textDecoration: 'none !important',
         "&:hover": {
