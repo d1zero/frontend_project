8ee9ecc115c54693cb1e4db4452087ca797a6b14
Alex Timofeev <asashek@yandex.ru>
2021-10-09 17:58:30 +0300

    Page title fix

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -15,7 +15,7 @@ SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 const MainPage = observer(() => {
     const classes = useStyles();
-    SetTitle("Main main page");
+    SetTitle("Main page");
     return (
         <div style={{ marginTop: "100px" }}>
             <Typography variant="h3" align="center">
