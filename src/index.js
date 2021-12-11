c7d32c35df547f559e5fa3e5291b5b177741e567
Alex Timofeev <alexsergeev45@gmail.com>
2021-12-11 08:17:15 +0300

    styled-components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import './index.scss'
+import "normalize.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    overflow-y: scroll;
+  }
+`;
 
 ReactDOM.render(
     <Router>
+        <GlobalStyles />
         <App />
     </Router>,
     document.getElementById("root"),
